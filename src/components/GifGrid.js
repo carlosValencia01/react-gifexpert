7876cda0f9f183a0778ed9c8f6fdf670c223fa01
Carlos Valencia <jocavalenciava@ittepic.edu.mx>
2022-01-06 17:24:11 -0700

    fix: paths again

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,31 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import { useFetchGifs } from '../hooks/useFetchGifs';
+import React, { useEffect, useState } from "react";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 // import { getGifs } from '../helpers/getGifs';
-import { GifGridItem } from './GifGridItem';
+import { GifGridItem } from "./GifGridItem";
 
-export const GifGrid = ({category}) => {
+export const GifGrid = ({ category }) => {
+  // const [images, setImages] = useState([]);
 
-    // const [images, setImages] = useState([]);
+  const { data, loading } = useFetchGifs(category);
+  console.log(data);
+  console.log(loading);
 
-
-
-    const {data, loading} = useFetchGifs( category );
-    console.log(data);
-    console.log(loading);
-    
-    return (
-        <>
-        <h3>{category}</h3>
-        {loading && <p>Loading...</p>}
-        <div className='card-grid'>
-            {
-                data.map(img=>(
-                    <GifGridItem 
-                        key={img.id}
-                        {...img} />
-                ))
-            }
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      <h3>{category}</h3>
+      {loading && <p>Loading...</p>}
+      <div className="card-grid">
+        {data.map((img) => (
+          <GifGridItem key={img.id} {...img} />
+        ))}
+      </div>
+    </>
+  );
+};
